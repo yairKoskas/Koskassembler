0dc40edb03ce35364794fb5a1554c90d8b7d67c0
yair koskas <yairi2003@gmail.com>
2021-07-15 19:56:55 +0300

    fixed testing

--- Tests/test_elf.py
+++ Tests/test_elf.py
@@ -4,12 +4,12 @@ import os
 
 def test_elf():
     for i in range(1, 5):
-        string = 'input_files/input' + str(i) + ' --info-header --info-plt --info-shstrtab ' \
+        string = 'Tests/input_files/input' + str(i) + ' --info-header --info-plt --info-shstrtab ' \
                                                 '--info-symtab --checksec '
         input_process = subprocess.Popen(['../koskassembler ' + string], stdout=subprocess.PIPE, shell=True)
         input_elf, err = input_process.communicate()
         input_elf = input_elf.decode()
-        with open('output_files/output' + str(i), 'r') as f:
+        with open('Tests/output_files/output' + str(i), 'r') as f:
             output_elf = f.read()
         input_elf = os.linesep.join([s for s in input_elf.splitlines() if s])
         output_elf = os.linesep.join([s for s in output_elf.splitlines() if s])
